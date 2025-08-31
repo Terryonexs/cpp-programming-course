993f6b90b5a30cd92142c94dca071dd75a962c67
Kvasha Maxim <terry683xs@gmail.com>
2025-08-31 21:56:26 +0300

    ДЗ №1:  Auto та range-based loops - Написав дві шаблонні функції

---
+++ homework-01/homework1.cpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <iostream>
+
+// range‑based loop
+template <typename Container>
+void print_range(const Container& cont)
+{
+    for (const auto& elem : cont)          
+        std::cout << elem << ' ';
+    std::cout << '\n';
+}
+
+// auto iterator
+
+template <typename Container>
+void print_iter(const Container& cont)
+{
+    for (auto it = cont.begin(); it != cont.end(); ++it)   
+        std::cout << *it << ' ';
+    std::cout << '\n';
+}
+  
